ff7e6a0561317cee09b5feb41b11463c1224002d
z115360 <157492975+z115360@users.noreply.github.com>
2024-02-27 13:43:29 +0100

    Add files via upload

--- aplha.js
+++ aplha.js
@@ -152,6 +152,20 @@
                  document.getElementById("ahorcado_6").hidden=true;
             }
             
+{
+    var seconds=0;
+	     		function timer(){
+
+			seconds=seconds+1;
+
+			document.getElementById("counter").innerHTML=seconds;
+
+
+
+	   	  }
+	     		setInterval(timer,1000);
+	
+}
  
             
 
